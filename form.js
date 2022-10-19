dc17c529c1c6bdc27a688f29b33fc25d60a6548c
unknown <honorineuwituze896@gmail.com>
2022-10-19 08:12:13 +0200

    new

---
+++ form.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var username = document.getElementById("username");
+    var email=document.getElementById("email");
+    var field1= document.getElementById("field1");
+    var field2= document.getElementById("field2");
+    field1.innerHTML="Please enter your name";
+    field2.innerHTML="Please enter your Email";
+    // document.getElementsByClassName(".input-control input").style.color= "red";
+    // var x= document.querySelectorAll(".in");
+}
